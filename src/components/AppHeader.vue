8ce485c724b0f997ab0f526d12bea95654397d78
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-13 12:09:36 +0100

    Add: component AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <nav>
+            <!-- order online -->
+            <a href="#" class="btn">
+
+            </a>
+
+            <!-- links -->
+            <ul>
+                <li>
+                    <a href="#">
+
+                    </a>
+                </li>
+            </ul>
+
+            <!-- cart and search -->
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="" alt="">
+                        
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
