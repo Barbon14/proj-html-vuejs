cc2933908ab11c45b71c74010c55d66440ee8ba6
BarbonFisso <barbieri.mat@hotmail.it>
2021-12-14 14:26:53 +0100

    Add: testimonials scetiojn in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,6 +8,19 @@
                 alt=""
             >
         </section>
+        <section id="testimonials">
+            <div>
+                <h2>
+                    "
+                </h2>
+                <h3>
+                    "forget the trendy pizza shops, this hidden spot makes the best new york-style pizza slice in naples"
+                </h3>
+                <h5>
+                    washington post 2018
+                </h5>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -26,6 +39,11 @@ main {
     .thumb {
         width: calc(25% - 10px);
         margin: 0 5px;
+    }
+
+    #testimonials {
+        background-image: url('../assets/img/h3-testimonials-bckgrnd.jpg');
+        height: 495px;
     }
 }
 </style>
